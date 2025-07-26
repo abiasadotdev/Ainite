9ed7ee0a7730d09b93d0fd689e613de8e277aace
unknown <abhiassadevv@gmail.com>
2025-07-26 22:28:19 +0700

    Replace generate key function to constructor method in wallet system

--- src/core/wallet.ts
+++ src/core/wallet.ts
@@ -4,18 +4,18 @@ import Ainite from "./index";
 
 import { priBase64, pubBase64 } from "./base64";
 
-const { privateKey, publicKey } = crypto.generateKeyPairSync("ec", {
-  namedCurve: "secp256k1",
-  publicKeyEncoding: { type: "spki", format: "pem" },
-  privateKeyEncoding: { type: "pkcs8", format: "pem" },
-});
-
 class Wallet {
   balance: number;
   privateKey: string;
   publicKey: string;
 
   constructor() {
+    const { privateKey, publicKey } = crypto.generateKeyPairSync("ec", {
+      namedCurve: "secp256k1",
+      publicKeyEncoding: { type: "spki", format: "pem" },
+      privateKeyEncoding: { type: "pkcs8", format: "pem" },
+    });
+
     this.privateKey = priBase64(privateKey);
     this.publicKey = pubBase64(publicKey);
     this.balance = 0;
